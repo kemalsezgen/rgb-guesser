d8baf84336e7b564ccf8d1f99d87281f8f5bc3fa
Kemal Sezgen <kemalsezgen@Kemal-MacBook-Pro.local>
2024-08-20 17:45:26 +0300

    footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <h3><a href='https://www.github.com/kemalsezgen' target="_blank" rel="noopener noreferrer">Github</a></h3>
+      <h3><a href='https://www.linkedin.com/in/kemalsezgen' target="_blank" rel="noopener noreferrer">LinkedIn</a></h3>
+    </div>
+  )
+}
+
+export default Footer
